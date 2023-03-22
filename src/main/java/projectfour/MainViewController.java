f35fbc835f56ed8447b7527b24ce689ee859c3a3
Owner <aryanjairath7@gmail.com>
2023-03-22 15:44:38 -0400

    tried different views

---
+++ src/main/java/projectfour/MainViewController.java
@@ -0,0 +1,13 @@
+package projectfour;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainViewController {
+
+}
