c9c030b7486c2d14890f06a5651432ced8e13fdb
Owner <aryanjairath7@gmail.com>
2023-03-21 22:52:09 -0400

    basic implementation

--- src/main/java/projectfour/Coffee.java
+++ src/main/java/projectfour/Coffee.java
@@ -1,4 +1,29 @@
 package projectfour;
 
+import java.util.ArrayList;
+
 public class Coffee {
+    int cupsize;
+    ArrayList<String> addIns;
+
+    public Coffee(int cupsize){
+        this.cupsize = cupsize;
+        addIns = new ArrayList<>();
+    }
+
+    public int getCupSize(){
+        return cupsize;
+    }
+
+    public void setCupsize(int cupsize){
+        this.cupsize = cupsize;
+    }
+    public void addaddIn(String addIn){
+        addIns.add(addIn);
+    }
+
+    public void removeaddIn(String addIn){
+        addIns.remove(addIn);
+    }
+
 }
--- src/main/java/projectfour/Order.java
+++ src/main/java/projectfour/Order.java
@@ -1,4 +1,28 @@
 package projectfour;
 
+import java.util.ArrayList;
+
 public class Order {
+
+    private int orderNumber;
+    private ArrayList<MenuItem> menuItems;
+
+    public Order(int orderNumber){
+        this.orderNumber = orderNumber;
+        menuItems = new ArrayList<>();
+    }
+
+    public void addItem(MenuItem item){
+        menuItems.add(item);
+    }
+
+    public void removeitem(MenuItem item){
+        menuItems.remove(item);
+    }
+
+    public int getOrderNumber(){
+        return this.orderNumber;
+    }
+
+
 }
